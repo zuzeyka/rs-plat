f8a0f6e335c0cea77ddef19c973d5d30f8e7f4b7
zuzeyka <stepan.tarasenko.26@gmail.com>
2025-03-29 20:03:18 +0100

    fix text color on the dark theme

--- src/components/Objects.tsx
+++ src/components/Objects.tsx
@@ -22,9 +22,9 @@ const Objects = () => {
     };
 
     return (
-        <section id="objects" className="w-full mx-auto border-b-4 border-primary py-20 text-center text-typography">
+        <section id="objects" className="w-full mx-auto border-b-4 border-primary py-20 text-center text-typography-dark">
             <h2 className="lg:text-7xl text-3xl mb-10">{t('objects.title')}</h2>
-            <h3 className="lg:text-3xl text-lg font-bold text-typography mb-4">{t('objects.subtitle')}</h3>
+            <h3 className="lg:text-3xl text-lg font-bold text-typography-dark mb-4">{t('objects.subtitle')}</h3>
 
             <div className="grid md:grid-cols-3 gap-6">
                 {objects.map((object, index) => (
@@ -37,18 +37,18 @@ const Objects = () => {
                         transition={{ duration: 0.5, delay: index * 0.2 }}
                         whileHover="hover"
                     >
-                        <h3 className="relative lg:absolute top-64 -left-36 text-typography text-xl font-bold transform -rotate-90 lg:opacity-0 whitespace-nowrap">
+                        <h3 className="relative lg:absolute top-64 -left-36 text-typography-dark text-xl font-bold transform -rotate-90 lg:opacity-0 whitespace-nowrap">
                             {object.title}
                         </h3>
                         <div className="relative lg:absolute -top-2 -left-22 flex items-center justify-center lg:opacity-0">
                             <MapPinIcon></MapPinIcon>
-                            <h4 className="text-typography text-lg font-bold transform">{object.city}</h4>
+                            <h4 className="text-typography-dark text-lg font-bold transform">{object.city}</h4>
                         </div>
                         <motion.div className="absolute inset-0 bg-black bg-opacity-50" variants={overlayVariants} transition={{ duration: 0.5 }}>
-                            <h3 className="relative top-60 -left-36 text-typography text-xl font-bold transform -rotate-90">{object.title}</h3>
+                            <h3 className="relative top-60 -left-36 text-typography-dark text-xl font-bold transform -rotate-90">{object.title}</h3>
                             <div className="relative -top-2 -left-22 flex items-center justify-center">
                                 <MapPinIcon></MapPinIcon>
-                                <h4 className="text-typography text-lg font-bold transform">{object.city}</h4>
+                                <h4 className="text-typography-dark text-lg font-bold transform">{object.city}</h4>
                             </div>
                         </motion.div>
                         <motion.img
