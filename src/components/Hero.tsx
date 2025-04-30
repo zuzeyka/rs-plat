6ef011f9347275dfc031149c137caaf0c49fdd5a
zuzeyka <stepan.tarasenko.26@gmail.com>
2025-04-30 23:46:19 +0200

    removed hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,11 +10,6 @@ const Hero = () => {
                 style={{ backgroundImage: "url('home.png')" }}
             ></div>
             <div className="absolute inset-0 z-0 bg-black w-full h-full"></div>
-            <h1 className="lg:text-7xl z-10 text-3xl font-bold font-monserrat mb-4">
-                <span className="block">{t('hero.title')}</span>
-                <span className="block">{t('hero.company')}</span>
-            </h1>
-            <p className="lg:text-2xl z-10 text-lg text-center">{t('hero.subtitle')}</p>
             <a
                 href="#contacts"
                 className="lg:text-2xl z-10 text-lg text-center bg-primary hover:bg-primary-dark rounded-xl mx-4 absolute bottom-10 text-typography-dark rounded-lg px-4 py-2 mt-6 hover:bg-primary-dark transition duration-300"
