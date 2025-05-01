8c931cd9f9f422a0845d83d68c1d1625045682b4
zuzeyka <stepan.tarasenko.26@gmail.com>
2025-05-01 22:15:52 +0200

    changed about text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,21 +9,12 @@ const About = () => {
         <section id="about" className="relative w-full text-center text-typography-dark">
             <div className="container lg:flex mx-auto p-10 lg:p-20 relative">
                 <div className="absolute inset-0 bg-cover bg-black opacity-50 rounded-3xl bg-center w-full h-full z-0"></div>
-                <div className="w-full z-10 relative">
-                    <h2 className="lg:text-7xl text-3xl font-bold z-10 font-montserrat mb-4">{t('about.title')}</h2>
-                    <h3 className="lg:text-3xl text-lg font-bold z-10 mb-4">{t('about.subtitle')}</h3>
-                    <p className="lg:text-2xl text-lg z-10 leading-relaxed mb-10">{t('about.description')}</p>
+                <div className="w-full z-10 p-4 relative">
+                    <p className="lg:text-2xl text-lg z-10 leading-relaxed">{t('about.subtitle')}</p>
                 </div>
 
-                <div className="w-full z-10 text-left p-4 text-left lg:p-10 bg-background rounded-lg relative">
-                    <h3 className="lg:text-3xl text-2xl text-center font-bold mb-6">{t('about.guaranteeTitle', 'Vi garanterar:')}</h3>
-                    <ul className="flex flex-col lg:pb-14 lg:items-center justify-between h-full lg:text-xl text-lg">
-                        {data.map((item: string, index: number) => (
-                            <li className={'list-decimal' + (index % 2 === 0 ? ' text-primary' : ' text-secondary')} key={index}>
-                                {item}
-                            </li>
-                        ))}
-                    </ul>
+                <div className="w-full z-10 p-4 relative">
+                    <p className="lg:text-2xl text-lg z-10 leading-relaxed">{t('about.description')}</p>
                 </div>
             </div>
         </section>
