e05a4c7e98d37c2fb3aeeda1fdbeb03d3b181351
zuzeyka <stepan.tarasenko.26@gmail.com>
2025-05-02 23:58:04 +0200

    added second logo to about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,14 +5,17 @@ const About = () => {
 
     return (
         <section id="about" className="relative w-full text-center text-typography-dark">
-            <div className="container lg:flex mx-auto p-10 lg:p-20 relative">
+            <div className="container py-8 lg:flex flex-col gap-6 relative">
                 <div className="absolute inset-0 bg-cover bg-black opacity-50 rounded-3xl bg-center w-full h-full z-0"></div>
-                <div className="w-full z-10 p-4 relative">
-                    <p className="lg:text-2xl text-lg z-10 leading-relaxed">{t('about.subtitle')}</p>
-                </div>
+                <img src="logo_runva_.svg" alt="logo" className="w-full h-96 object-cover" />
+                <div className="lg:flex relative">
+                    <div className="w-full z-10 p-4 relative">
+                        <p className="lg:text-2xl text-lg z-10 leading-relaxed">{t('about.subtitle')}</p>
+                    </div>
 
-                <div className="w-full z-10 p-4 relative">
-                    <p className="lg:text-2xl text-lg z-10 leading-relaxed">{t('about.description')}</p>
+                    <div className="w-full z-10 p-4 relative">
+                        <p className="lg:text-2xl text-lg z-10 leading-relaxed">{t('about.description')}</p>
+                    </div>
                 </div>
             </div>
         </section>
