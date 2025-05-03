b5b5bc33852713abf9ac4faad98d1ca5cf280f77
zuzeyka <stepan.tarasenko.26@gmail.com>
2025-05-03 23:45:20 +0200

    changed second logo to about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,10 +4,12 @@ const About = () => {
     const { t } = useTranslation();
 
     return (
-        <section id="about" className="relative w-full text-center text-typography-dark">
+        <section
+            id="about"
+            className="relative w-full shadow-lg hover:shadow-2xl transition-shadow  dark:bg-typography bg-typography-dark rounded-3xl text-center text-typography dark:text-typography-dark"
+        >
             <div className="container py-8 lg:flex flex-col gap-6 relative">
-                <div className="absolute inset-0 bg-cover bg-black opacity-50 rounded-3xl bg-center w-full h-full z-0"></div>
-                <img src="logo_runva_.svg" alt="logo" className="w-full h-96 object-cover" />
+                <img src="logo_runva.svg" alt="logo" className="w-full" />
                 <div className="lg:flex relative">
                     <div className="w-full z-10 p-4 relative">
                         <p className="lg:text-2xl text-lg z-10 leading-relaxed">{t('about.subtitle')}</p>
